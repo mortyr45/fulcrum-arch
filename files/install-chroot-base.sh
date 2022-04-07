47e73b9c34d139b92bb0986e7a289cf1e560b0f2
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-07 15:18:11 +0200

    Update install-chroot-base.sh

--- files/install-chroot-base.sh
+++ files/install-chroot-base.sh
@@ -12,6 +12,3 @@ locale-gen
 echo "$SCRIPT_HOSTNAME" > /etc/hostname
 grub-install --target=x86_64-efi --bootloader-id=$SCRIPT_BOOTLOADER_ID
 cp /usr/share/locale/$SCRIPT_GRUB_LANG\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/$SCRIPT_GRUB_LANG.mo
-
-mkinitcpio -P
-grub-mkconfig -o /boot/grub/grub.cfg

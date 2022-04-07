9905f512e46b80455dff7b748a182c77d2bad1c9
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-07 15:32:18 +0200

    Create install-kernel.sh

---
+++ files/install-kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+printf "Which kernel(s) would you like to install?\n1) linux\n2) linux-lts\n3) linux-hardened\n4) linux-zen\n"
+echo -n "Choose multiple of them, by separating the numbers with a ','[1]: "
+read;
+if [ -z $REPLY ] ; then
+  arch-chroot /mnt "pacman -S linux linux-firmware linux-headers"
+else
+  TEMP=""
+  IFS=","
+  for KERNEL in $REPLY ; do
+    case $KERNEL in
+    1)
+      TEMP+=" linux linux-firmware linux-headers" ;;
+    2)
+      TEMP+=" linux-lts linux-lts-firmware linux-lts-headers" ;;
+    3)
+      TEMP+=" linux-hardened linux-hardened-firmware linux-hardened-headers" ;;
+    4)
+      TEMP+=" linux-zen linux-zen-firmware linux-zen-headers" ;;
+  done
+  arch-chroot /mnt pacman --noconfirm -S $TEMP
+fi

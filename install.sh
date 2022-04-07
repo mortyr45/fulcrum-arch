a80ad156f68a26a6c5f35e4a82a858b2ef0720b3
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-07 14:11:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,16 @@ timedatectl set-ntp true
 pacstrap /mnt base linux linux-firmware linux-headers btrfs-progs nano grub efibootmgr os-prober
 genfstab -U /mnt > /mnt/etc/fstab
 
-curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/chroot.sh >> /mnt/install.sh
-sed -ri -e "s!^SCRIPT_TIMEZONE=!SCRIPT_TIMEZONE=$SCRIPT_TIMEZONE!g" /mnt/install.sh
-sed -ri -e "s!^SCRIPT_HOSTNAME=!SCRIPT_HOSTNAME=$SCRIPT_HOSTNAME!g" /mnt/install.sh
-sed -ri -e "s!^SCRIPT_BOOTLOADER_ID=!SCRIPT_BOOTLOADER_ID=$SCRIPT_BOOTLOADER_ID!g" /mnt/install.sh
-sed -ri -e "s!^SCRIPT_GRUB_LANG=!SCRIPT_GRUB_LANG=$SCRIPT_GRUB_LANG!g" /mnt/install.sh
-chmod +x /mnt/install.sh
-arch-chroot /mnt ./install.sh
+CHROOT_INSTALL_FILES=("base" "kernel")
+for FILE in ${CHROOT_INSTALL_FILES} ; do
+  ! [ -f "install-$FILE.sh" ] && curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/install-chroot-$FILE.sh > install-$FILE.sh
+  [ $? != 0 ] && exit 1
+  chmod +x /mnt/install-$FILE.sh
+done
+
+sed -ri -e "s!^SCRIPT_TIMEZONE=!SCRIPT_TIMEZONE=$SCRIPT_TIMEZONE!g" /mnt/install-chroot-base.sh
+sed -ri -e "s!^SCRIPT_HOSTNAME=!SCRIPT_HOSTNAME=$SCRIPT_HOSTNAME!g" /mnt/install-chroot-base.sh
+sed -ri -e "s!^SCRIPT_BOOTLOADER_ID=!SCRIPT_BOOTLOADER_ID=$SCRIPT_BOOTLOADER_ID!g" /mnt/install-chroot-base.sh
+sed -ri -e "s!^SCRIPT_GRUB_LANG=!SCRIPT_GRUB_LANG=$SCRIPT_GRUB_LANG!g" /mnt/install-chroot-base.sh
+
+arch-chroot /mnt

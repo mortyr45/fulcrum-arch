6d56eba3e858d50f1ab02ada90cf1552406d38dc
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-07 15:31:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,10 +23,20 @@ read;
 if [ -z $REPLY ] ; then
   arch-chroot /mnt "pacman -S linux linux-firmware linux-headers"
 else
+  TEMP=""
   IFS=","
   for KERNEL in $REPLY ; do
-    arch-chroot /mnt pacman --noconfirm -S $KERNEL $KERNEL-firmware $KERNEL-headers
+    case $KERNEL in
+    1)
+      TEMP+=" linux linux-firmware linux-headers" ;;
+    2)
+      TEMP+=" linux-lts linux-lts-firmware linux-lts-headers" ;;
+    3)
+      TEMP+=" linux-hardened linux-hardened-firmware linux-hardened-headers" ;;
+    4)
+      TEMP+=" linux-zen linux-zen-firmware linux-zen-headers" ;;
   done
+  arch-chroot /mnt pacman --noconfirm -S $TEMP
 fi
 
 CHROOT_INSTALL_FILES=("base")

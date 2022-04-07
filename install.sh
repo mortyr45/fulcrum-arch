a38d6d60a44f86fb7d5c0ebec43bbca4a5a2ed1c
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-07 15:15:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ pacstrap /mnt base btrfs-progs nano grub efibootmgr os-prober
 genfstab -U /mnt > /mnt/etc/fstab
 
 clear
-printf "Which kernel(s) would you like to install?\n1) linux\n2) linux-lts\n3) linux-hardened\n4) linux-zen"
+printf "Which kernel(s) would you like to install?\n1) linux\n2) linux-lts\n3) linux-hardened\n4) linux-zen\n"
 echo -n "Choose multiple of them, by separating the numbers with a ','[1]: "
 read;
 if [ -z $REPLY ] ; then
@@ -25,13 +25,13 @@ if [ -z $REPLY ] ; then
 else
   IFS=","
   for KERNEL in $REPLY ; do
-    arch-chroot /mnt "pacman -S $KERNEL $KERNEL-firmware $KERNEL-headers"
+    arch-chroot /mnt pacman --noconfirm -S $KERNEL $KERNEL-firmware $KERNEL-headers
   done
 fi
 
-CHROOT_INSTALL_FILES=("base" "kernel")
+CHROOT_INSTALL_FILES=("base")
 for FILE in ${CHROOT_INSTALL_FILES} ; do
-  ! [ -f "install-$FILE.sh" ] && curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/install-chroot-$FILE.sh > install-$FILE.sh
+  ! [ -f "install-$FILE.sh" ] && curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/install-chroot-$FILE.sh > /mnt/install-$FILE.sh
   [ $? != 0 ] && exit 1
   chmod +x /mnt/install-$FILE.sh
 done

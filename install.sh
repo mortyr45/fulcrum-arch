8184d71cf06bd655819e06da47fe67a148f56d1f
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-08 10:24:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,13 @@ source install-partitions.sh
 timedatectl set-ntp true
 case $SCRIPT_CPU_MITIGATIONS in
   0)
-    SCRIPT_CPU_MITIGATIONS=" linux-lts linux-lts-headers" ;;
+    SCRIPT_CPU_MITIGATIONS="" ;;
   1)
-    SCRIPT_CPU_MITIGATIONS=" linux linux-headers" ;;
+    SCRIPT_CPU_MITIGATIONS=" amd-ucode" ;;
   2)
-    SCRIPT_CPU_MITIGATIONS=" linux-hardened linux-hardened-headers" ;;
+    SCRIPT_CPU_MITIGATIONS=" intel-ucode" ;;
 esac
-pacstrap /mnt base btrfs-progs nano grub efibootmgr os-prober
+pacstrap /mnt base btrfs-progs nano grub efibootmgr os-prober $SCRIPT_CPU_MITIGATIONS
 genfstab -U /mnt > /mnt/etc/fstab
 
 source install-kernel.sh

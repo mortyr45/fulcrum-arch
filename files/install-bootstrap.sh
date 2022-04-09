adecc3a31410935c6811c52597ed824c10639142
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-09 20:24:51 +0200

    Update install-bootstrap.sh

--- files/install-bootstrap.sh
+++ files/install-bootstrap.sh
@@ -30,3 +30,10 @@ done
 
 arch-chroot /mnt useradd -m -G wheel $SCRIPT_USERNAME
 echo $SCRIPT_USERNAME:$SCRIPT_PASSWORD | arch-chroot /mnt chpasswd
+
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/$SCRIPT_TIMEZONE /etc/localtime
+arch-chroot /mnt sed -ri -e "s!^#$SCRIPT_LOCALE!$SCRIPT_LOCALE!g" /etc/locale.gen
+arch-chroot /mnt locale-gen
+echo "$SCRIPT_HOSTNAME" > /mnt/etc/hostname
+arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=$SCRIPT_BOOTLOADER_ID
+arch-chroot /mnt cp /usr/share/locale/$SCRIPT_GRUB_LANG\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/$SCRIPT_GRUB_LANG.mo

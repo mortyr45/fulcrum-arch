40472b1cd57d2f4a475c1349795bc91dd0cfc697
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-09 16:50:10 +0200

    Update install-bootstrap.sh

--- files/install-bootstrap.sh
+++ files/install-bootstrap.sh
@@ -11,3 +11,17 @@ case $SCRIPT_CPU_MITIGATIONS in
 esac
 pacstrap /mnt base btrfs-progs nano grub efibootmgr os-prober sudo $SCRIPT_CPU_MITIGATIONS
 genfstab -U /mnt > /mnt/etc/fstab
+
+while true ; do
+  read -p "Username:
+  ! [ -z $REPLY ] break
+done
+SCRIPT_USERNAME=$REPLY
+
+while true ; do
+  read -sp "Password:"
+  ! [ -z $REPLY ] break
+done
+SCRIPT_PASSWORD=$REPLY
+
+echo "$SCRIPT_USERNAME:$SCRIPT_PASSWORD"

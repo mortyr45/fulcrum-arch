44c9b552771df82227318b219adb03cb25a76463
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-09 19:24:09 +0200

    Update install-prompt.sh

--- files/install-prompt.sh
+++ files/install-prompt.sh
@@ -8,66 +8,67 @@ while true ; do
   [ $? == 0 ] && EFI_INSTALL=1
   
   SCRIPT_EFI_PARTITION=/dev/sda1
-  echo -n "Device and partition number for EFI[$SCRIPT_EFI_PARTITION]:"
+  echo -n "Device and partition number for EFI [$SCRIPT_EFI_PARTITION]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_EFI_PARTITION=$REPLY
 
   SCRIPT_ROOT_PARTITION=/dev/sda2
-  echo -n "Device and partition number for root[$SCRIPT_ROOT_PARTITION]:"
+  echo -n "Device and partition number for root [$SCRIPT_ROOT_PARTITION]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_ROOT_PARTITION=$REPLY
   
   SCRIPT_ROOT_PARTITION_SSD=true
-  echo -n "Is the root partition on an ssd?[Y/n]:"
+  echo -n "Is the root partition on an ssd? [Y/n]: "
   read;
   [ "$REPLY" == "n" ] && SCRIPT_ROOT_PARTITION_SSD=false
 
   SCRIPT_TIMEZONE_REGION=Asia
   ls /usr/share/zoneinfo
-  echo -n "Time zone region[$SCRIPT_TIMEZONE_REGION]:"
+  echo -n "Time zone region [$SCRIPT_TIMEZONE_REGION]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_TIMEZONE_REGION=$REPLY
 
   SCRIPT_TIMEZONE_CITY=Tokyo
   ls /usr/share/zoneinfo/$SCRIPT_TIMEZONE_REGION
-  echo -n "Time zone region[$SCRIPT_TIMEZONE_CITY]:"
+  echo -n "Time zone region [$SCRIPT_TIMEZONE_CITY]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_TIMEZONE_CITY=$REPLY
 
   SCRIPT_TIMEZONE="$SCRIPT_TIMEZONE_REGION/$SCRIPT_TIMEZONE_CITY"
   
   SCRIPT_LOCALE=en_US.UTF-8
-  echo -n "Locale: [$SCRIPT_LOCALE]:"
+  echo -n "Locale [$SCRIPT_LOCALE]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_LOCALE=$REPLY
 
   SCRIPT_HOSTNAME=arch
-  echo -n "System hostname[$SCRIPT_HOSTNAME]:"
+  echo -n "System hostname [$SCRIPT_HOSTNAME]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_HOSTNAME=$REPLY
 
   SCRIPT_BOOTLOADER_ID=GRUB
-  echo -n "Identifier in the bootloader[$SCRIPT_BOOTLOADER_ID]:"
+  echo -n "Identifier in the bootloader [$SCRIPT_BOOTLOADER_ID]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_BOOTLOADER_ID=$REPLY
 
   SCRIPT_GRUB_LANG=en
-  echo -n "Language in the bootloader[$SCRIPT_GRUB_LANG]:"
+  echo -n "Language in the bootloader [$SCRIPT_GRUB_LANG]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_GRUB_LANG=$REPLY
   
   SCRIPT_KERNEL="1"
   printf "Which kernel(s) would you like to install?\n1) linux-lts\n2) linux\n3) linux-hardened\n4) linux-zen\n"
-  echo -n "Choose multiple of them, by separating the numbers with a ','[$SCRIPT_KERNEL]: "
+  echo -n "Choose multiple of them, by separating the numbers with a ',' [$SCRIPT_KERNEL]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_KERNEL=$REPLY
   
   SCRIPT_CPU_MITIGATIONS="0"
   printf "Whicch cpu microcode package would you like to install?\n0) none\n1) amd-ucode\n2) intel-ucode"
-  echo -n "Please choose cpu microcode mitigation to be installed[$SCRIPT_CPU_MITIGATIONS]: "
+  echo -n "Please choose cpu microcode mitigation to be installed [$SCRIPT_CPU_MITIGATIONS]: "
   read;
   ! [ -z $REPLY ] && SCRIPT_CPU_MITIGATIONS=$REPLY
 
+  clear
   echo "EFI partition: $SCRIPT_EFI_PARTITION"
   echo "Root partition: $SCRIPT_ROOT_PARTITION"
   echo "Timezone: $SCRIPT_TIMEZONE"
@@ -77,7 +78,7 @@ while true ; do
   echo "Grub language: $SCRIPT_GRUB_LANG"
   echo "Chosen kernel(s): $SCRIPT_KERNEL"
   echo "Chosen cpu microcode mitigation: $SCRIPT_CPU_MITIGATIONS"
-  echo -n "Are the settings correct?[y/N]"
+  echo -n "Are the settings correct? [y/N]: "
   read;
   [ $REPLY == "y" ] && break
 done

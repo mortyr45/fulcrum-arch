05380aa59d1e6a358582de19e90a60771b15a6ea
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-09 22:46:46 +0200

    Update install-bootstrap.sh

--- files/install-bootstrap.sh
+++ files/install-bootstrap.sh
@@ -11,3 +11,7 @@ case $SCRIPT_CPU_MITIGATIONS in
 esac
 pacstrap /mnt base btrfs-progs nano grub efibootmgr os-prober sudo $SCRIPT_CPU_MITIGATIONS
 genfstab -U /mnt > /mnt/etc/fstab
+
+arch-chroot /mnt sed -ri -e "s!*GRUB_TIMEOUT=!GRUB_TIMEOUT=3!g" /etc/default/grub
+arch-chroot /mnt sed -ri -e "s!*GRUB_DEFAULT=!GRUB_DEFAULT=saved!g" /etc/default/grub
+arch-chroot /mnt sed -ri -e "s!*GRUB_SAVEDEFAULT=!GRUB_SAVEDEFAULT=true!g" /etc/default/grub

821213dc55012da4a061cfb1dd7476228e67d1da
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-10 10:38:37 +0200

    Update install-chroot-test.sh

--- files/install-chroot-test.sh
+++ files/install-chroot-test.sh
@@ -3,6 +3,7 @@
 # Security
 echo "Defaults editor=/usr/bin/rnano" >> /etc/sudoers
 passwd --lock root
+
 pacman -S ufw
 systemctl disable iptables
 systemctl enable ufw
@@ -11,3 +12,6 @@ ufw default deny forward
 ufw default allow outgoing
 ufw allow from 192.168.0.0/23
 ufw limit 22/tcp
+
+# Packages
+

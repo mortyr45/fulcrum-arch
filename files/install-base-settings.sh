af1cfeabf6e99a13c0689cfe6945bf3355f6c2eb
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-10 22:07:10 +0200

    Update install-base-settings.sh

--- files/install-base-settings.sh
+++ files/install-base-settings.sh
@@ -28,3 +28,6 @@ arch-chroot /mnt locale-gen
 echo "$SCRIPT_HOSTNAME" > /mnt/etc/hostname
 arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=$SCRIPT_BOOTLOADER_ID
 arch-chroot /mnt cp /usr/share/locale/$SCRIPT_GRUB_LANG\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/$SCRIPT_GRUB_LANG.mo
+
+arch-chroot /mnt pacman -S networkmanager
+arch-chroot /mnt systemctl enable NetworkManager

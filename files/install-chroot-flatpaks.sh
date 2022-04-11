bfca5872b4d273088ca62ec3fc2a271bfe8c744d
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-11 15:01:44 +0200

    Update install-chroot-flatpaks.sh

--- files/install-chroot-flatpaks.sh
+++ files/install-chroot-flatpaks.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-flatpak install
+flatpak install \
+com.github.tchx84.Flatseal \
+de.haeckerfelix.Shortwave \
+dev.geopjr.Hashbrown \
+org.gnome.Evince \
+org.gnome.gedit \
+org.gnome.Calculator \
+org.gnome.Calendar \
+org.gnome.Characters \
+org.gnome.clocks \
+org.gnome.Firmware \
+org.gnome.Logs \
+org.gnome.gThumb \
+org.gnome.gitlab.somas.Apostrophe \
+org.gnome.gitlab.somas.Apostrophe.Plugin.TexLive \
+org.mozilla.firefox

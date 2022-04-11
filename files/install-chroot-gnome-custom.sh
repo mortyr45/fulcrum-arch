5750392ead261356970baf07cb90534fab058798
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-11 09:56:09 +0200

    Update install-chroot-gnome-custom.sh

--- files/install-chroot-gnome-custom.sh
+++ files/install-chroot-gnome-custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pacman -S gdm gnome-shell gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-terminal
+pacman -S gdm gnome-shell gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-terminal noto-fonts-emoji pipewire-media-session
 systemctl enable gdm

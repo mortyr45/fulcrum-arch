37e1344de2af3ae91f9f895f6b48758b519dc107
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-11 13:58:31 +0200

    Update install-chroot-gnome-custom.sh

--- files/install-chroot-gnome-custom.sh
+++ files/install-chroot-gnome-custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pacman -S alacritty evince file-roller firefox gdm gedit gnome-shell gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-keyring gnome-logs gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-terminal gnome-tweaks grilo-plugins gthumb gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb jack2 mutter nautilus noto-fonts noto-fonts-emoji pipewire-media-session
+pacman -S alacritty evince file-roller firefox gdm gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-terminal gnome-tweaks grilo-plugins gthumb gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb jack2 mutter nautilus noto-fonts noto-fonts-emoji pipewire-media-session
 systemctl enable gdm

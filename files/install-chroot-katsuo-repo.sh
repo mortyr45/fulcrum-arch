808abf922fb7880b88f1816326fe5a0601a2b816
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-12 16:19:47 +0200

    Update install-chroot-katsuo-repo.sh

--- files/install-chroot-katsuo-repo.sh
+++ files/install-chroot-katsuo-repo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "" >> /etc/pacman.conf
 echo "[katsuo]" >> /etc/pacman.conf
 echo "Server = https://pacman.katsuo.fish/core/$arch" >> /etc/pacman.conf
 echo "SigLevel = PackageRequired" >> /etc/pacman.conf

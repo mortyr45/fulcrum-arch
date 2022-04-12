5f379d977e67519d10792adde67c6643731c5863
Miklós Kovács <22072709+mortyr45@users.noreply.github.com>
2022-04-12 16:17:17 +0200

    Update install-chroot-chaotic-aur.sh

--- files/install-chroot-chaotic-aur.sh
+++ files/install-chroot-chaotic-aur.sh
@@ -5,3 +5,4 @@ pacman-key --lsign-key FBA220DFC880C036
 pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo "[chaotic-aur]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+pacman -Sy

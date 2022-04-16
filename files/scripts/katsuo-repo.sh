0232482bd6b8996d83432ca355fcd4905935a148
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-04-16 15:02:38 +0200

    Fixed weak string literal

--- files/scripts/katsuo-repo.sh
+++ files/scripts/katsuo-repo.sh
@@ -2,5 +2,5 @@
 
 echo "" >> /etc/pacman.conf
 echo "[katsuo]" >> /etc/pacman.conf
-echo "Server = https://pacman.katsuo.fish/core/$arch" >> /etc/pacman.conf
+echo 'Server = https://pacman.katsuo.fish/core/$arch' >> /etc/pacman.conf
 echo "SigLevel = PackageRequired" >> /etc/pacman.conf

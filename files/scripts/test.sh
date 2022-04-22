e1d2d3cbcd81af65324cadcf2e97fe3190362c7a
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-04-22 13:20:09 +0200

    Updated test.sh

--- files/scripts/test.sh
+++ files/scripts/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Other
-pacman --noconfirm -S zsh openssh gufw
+pacman --noconfirm -S gufw grub-btrfs openssh timeshift timeshift-autosnap zsh
 systemctl enable sshd
+timeshift --list
+systemctl enable grub-btrfs.path
 chsh -s /bin/zsh fulcrum
 sed -ri -e "s/^.*set\ softwrap.*/set\ softwrap/g" /etc/nanorc
+sed -ri -e "s/^updateGrub=true.*/updateGrub=false/g" /etc/timeshift-autosnap.conf
 
 #in /etc/pacman.d/mirrorlist set the desired mirror
 #pacman -Syyu

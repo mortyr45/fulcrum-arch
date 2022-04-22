6e50e4aed9bb85630d8722febe8cedd6aa32ffdb
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-04-22 14:13:02 +0200

    Fixed katsuo repo url

--- files/scripts/pacman.sh
+++ files/scripts/pacman.sh
@@ -28,7 +28,7 @@ fn_enable_multilib() {
 fn_katsuo_repo() {
     echo "" >> /etc/pacman.conf
     echo "[katsuo]" >> /etc/pacman.conf
-    echo 'Server = https://pacman.katsuo.fish/core/$arch' >> /etc/pacman.conf
+    echo 'Server = https://pacman.katsuo.fish/$arch' >> /etc/pacman.conf
     echo "SigLevel = PackageRequired" >> /etc/pacman.conf
 }
 

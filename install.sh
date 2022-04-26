0a7934af4ee3cc575fe5e88e876e2803545cc70d
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-04-26 11:15:57 +0200

    Removed temporary things

--- install.sh
+++ install.sh
@@ -154,13 +154,7 @@ grub_config() {
 # bash <(curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/scripts/pacman.sh) auto
 
 read -p "Do you want to run disk setup? [y/N]: "
-if [ "$REPLY" == "y" ] ; then
-	curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/disk-setup.sh > disk-setup.sh
-	/bin/bash disk-setup.sh
-	rm disk-setup.sh
-fi
-exit
-#[ "$REPLY" == "y" ] && bash <(curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/disk-setup.sh)
+[ "$REPLY" == "y" ] && bash <(curl -sL https://raw.githubusercontent.com/mortyr45/fulcrum-arch/master/files/disk-setup.sh)
 
 prompts
 bootstrap

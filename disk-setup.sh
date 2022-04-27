daccc2fc92c289e9e1dc4b121bae3c4016e668ce
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-04-27 18:40:35 +0200

    disk-setup.sh
    Changed basic partition layout to have separate boot partition.

--- disk-setup.sh
+++ disk-setup.sh
@@ -49,7 +49,8 @@ EOF
 
 fn_setup_btrfs_subvolumes() {
     BTRFS_PARTITION=$1
-    EFI_PARTITION=$2
+    BOOT_PARTITION=$2
+    EFI_PARTITION=$3
     mount $BTRFS_PARTITION /mnt
     btrfs subvolume create /mnt/@
     btrfs subvolume create /mnt/@home
@@ -63,16 +64,14 @@ mount $BTRFS_PARTITION -o subvol=@ /mnt
 mount --mkdir $BTRFS_PARTITION -o subvol=@home /mnt/home
 mount --mkdir $BTRFS_PARTITION -o subvol=@cache /mnt/var/cache
 mount --mkdir $BTRFS_PARTITION -o subvol=@log /mnt/var/log
+mount --mkdir $BOOT_PARTITION /mnt/boot
 mount --mkdir $EFI_PARTITION /mnt/boot/EFI
 EOF
 }
 
-fn_generate_hook_post_grub() {
+fn_generate_hook_post_crypttab_initramfs() {
     DRIVE_UUID=$(blkid -s UUID -o value $1)
-cat > post-install-hook.sh<< EOF
-arch-chroot /mnt sed -ri -e "s/^.*GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/g" /etc/default/grub
-arch-chroot /mnt sed -ri -e "s/^.*GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$DRIVE_UUID:luks_root\"/g" /etc/default/grub
-EOF
+    echo "luks_root /dev/disk/by-uuid/$DRIVE_UUID none defaults" > /mnt/etc/crypttab.initramfs
 }
 
 #####
@@ -88,8 +87,10 @@ fn_simple_btrfs() {
     fn_create_efi_partition $DRIVE_TO_USE
     mkfs.fat -F 32 "${DRIVE_TO_USE}1"
     fn_create_linux_partition $DRIVE_TO_USE
-    mkfs.btrfs "${DRIVE_TO_USE}2"
-    fn_setup_btrfs_subvolumes "${DRIVE_TO_USE}2" "${DRIVE_TO_USE}1"
+    mkfs.ext4 "${DRIVE_TO_USE}2"
+    fn_create_linux_partition $DRIVE_TO_USE
+    mkfs.btrfs "${DRIVE_TO_USE}3"
+    fn_setup_btrfs_subvolumes "${DRIVE_TO_USE}3" "${DRIVE_TO_USE}2" "${DRIVE_TO_USE}1"
 }
 
 fn_encrypted_btrfs() {
@@ -101,17 +102,19 @@ fn_encrypted_btrfs() {
     fn_create_efi_partition $DRIVE_TO_USE
     mkfs.fat -F 32 "${DRIVE_TO_USE}1"
     fn_create_linux_partition $DRIVE_TO_USE
-    cryptsetup -y --type luks1 luksFormat "${DRIVE_TO_USE}2"
-    cryptsetup open "${DRIVE_TO_USE}2" luks_root
+    mkfs.ext4 "${DRIVE_TO_USE}2"
+    fn_create_linux_partition $DRIVE_TO_USE
+    cryptsetup luksFormat "${DRIVE_TO_USE}3"
+    cryptsetup open "${DRIVE_TO_USE}3" luks_root
     mkfs.btrfs /dev/mapper/luks_root
-    fn_setup_btrfs_subvolumes /dev/mapper/luks_root "${DRIVE_TO_USE}1"
-    fn_generate_hook_post_grub "${DRIVE_TO_USE}2"
+    fn_setup_btrfs_subvolumes /dev/mapper/luks_root "${DRIVE_TO_USE}2" "${DRIVE_TO_USE}1"
+    fn_generate_hook_post_grub "${DRIVE_TO_USE}3"
 }
 
 DISK_SETUP_CHOICE="1"
-echo "1) Simple btrfs (1 EFI partition and 1 btrfs partition)"
-echo "2) Encrypted btrfs (1 EFI partition and 1 encrypted btrfs partition)"
-echo "3) Encrypted btrfs with detached header (1 EFI partition, 1 /boot partition and 1 encrypted btrfs partition)"
+echo "1) Simple btrfs (1 EFI partition, 1 /boot partition and 1 btrfs partition)"
+echo "2) Encrypted btrfs (1 EFI partition, 1 /boot partition and 1 encrypted btrfs partition)"
+echo "3) Encrypted btrfs with detached header (1 EFI partition, 1 /boot partition and 1 encrypted btrfs partition with detached luks header)"
 read -p "Choose disk setup method [$DISK_SETUP_CHOICE]: "
 ! [ -z $REPLY ] && DISK_SETUP_CHOICE=$REPLY
 

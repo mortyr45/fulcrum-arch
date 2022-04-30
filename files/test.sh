cdfbd0061e722d19619ed9d67b754fdb5c64b4d2
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-04-30 23:17:14 +0200

    Upd

--- files/test.sh
+++ files/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Other
-pacman --noconfirm -S auto-cpufreq ufw gnome-shell-extension-installer gufw openssh zsh
+pacman --noconfirm -S ufw gufw openssh zsh
 systemctl enable sshd.service
 chsh -s /bin/zsh fulcrum
 sed -ri -e "s/^.*set\ softwrap.*/set\ softwrap/g" /etc/nanorc

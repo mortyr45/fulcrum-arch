4db14945ba4a0a478697289896f7042744af9843
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-05-03 14:14:40 +0200

    disk-setup.sh

--- disk-setup.sh
+++ disk-setup.sh
@@ -27,24 +27,6 @@ fn_setup_btrfs_subvolumes() {
     echo "mount --mkdir $EFI_PARTITION /mnt/boot/EFI" >> pre-install-hook.sh
 }
 
-fn_generate_hook_post_grub() {
-    echo "dd if=/dev/urandom of=/mnt/crypto_keyfile.bin bs=1024 count=4" >> post-install-hook.sh
-    echo "chmod 400 /mnt/crypto_keyfile.bin" >> post-install-hook.sh
-    echo "cryptsetup luksAddKey /dev/$ROOT_PARTITION /mnt/crypto_keyfile.bin" >> post-install-hook.sh
-    echo "sed -ri -e \"s/^#GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/g\" /mnt/etc/default/grub" >> post-install-hook.sh
-    echo "sed -ri -e \"s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX='cryptdevice=UUID=$1:luks_root'/g\" /mnt/etc/default/grub" >> post-install-hook.sh
-    echo "echo \"luks_root /dev/disk/by-uuid/$1 /crypto_keyfile.bin luks \" > /mnt/etc/crypttab.initramfs" >> post-install-hook.sh
-    echo "sed -ri -e \"s/^FILES=.*/FILES=(\/crypto_keyfile.bin)/g\" /mnt/etc/mkinitcpio.conf"  >> post-install-hook.sh
-}
-
-fn_generate_hook_post_crypttab_initramfs() {
-    PARTITION_PATH=$(ls -l /dev/disk/by-path | grep $1 | cut -d ' ' -f 9)
-    CRYPT_OPTION="luks"
-    ! [ -z $2 ] && CRYPT_OPTION="header=/luks_root_header.img:UUID=$2"
-    echo "echo \"luks_root /dev/disk/by-path/$PARTITION_PATH none $CRYPT_OPTION\" > /mnt/etc/crypttab.initramfs" >> post-install-hook.sh
-    ! [ -z $2 ] && echo "cp luks_root_header.img /mnt/boot" >> post-install-hook.sh
-}
-
 #####
 # Control functions
 #####
@@ -68,20 +50,29 @@ fn_setup_boot_partition() {
 fn_setup_encrypted_root() {
     cryptsetup luksFormat --type luks1 /dev/$ROOT_PARTITION
     cryptsetup open /dev/$ROOT_PARTITION luks_root
-    fn_generate_hook_post_grub $(blkid -s UUID -o value /dev/$ROOT_PARTITION)
+    ROOT_UUID=$(blkid -s UUID -o value /dev/$ROOT_PARTITION)
+    echo "dd if=/dev/urandom of=/mnt/crypto_keyfile.bin bs=1024 count=4" >> post-install-hook.sh
+    echo "chmod 400 /mnt/crypto_keyfile.bin" >> post-install-hook.sh
+    echo "cryptsetup luksAddKey /dev/$ROOT_PARTITION /mnt/crypto_keyfile.bin" >> post-install-hook.sh
+    echo "echo \"luks_root /dev/disk/by-uuid/$ROOT_UUID /crypto_keyfile.bin luks \" > /mnt/etc/crypttab.initramfs" >> post-install-hook.sh
+    echo "sed -ri -e \"s/^FILES=.*/FILES=(\/crypto_keyfile.bin)/g\" /mnt/etc/mkinitcpio.conf"  >> post-install-hook.sh
 }
 
 fn_setup_encrypted_root_separate() {
     cryptsetup luksFormat /dev/$ROOT_PARTITION
     cryptsetup open /dev/$ROOT_PARTITION luks_root
-    fn_generate_hook_post_crypttab_initramfs $ROOT_PARTITION
+    ROOT_UUID=$(blkid -s UUID -o value /dev/$ROOT_PARTITION)
+    echo "echo \"luks_root /dev/disk/by-uuid/$ROOT_UUID none luks\" > /mnt/etc/crypttab.initramfs" >> post-install-hook.sh
 }
 
 fn_setup_encrypted_root_detached() {
     dd if=/dev/zero of=luks_root_header.img bs=16M count=1
     cryptsetup luksFormat /dev/$ROOT_PARTITION --offset 32768 --header luks_root_header.img
     cryptsetup open --header luks_root_header.img /dev/$ROOT_PARTITION luks_root
-    fn_generate_hook_post_crypttab_initramfs $ROOT_PARTITION $(blkid -s UUID -o value $BOOT_PATH)
+    BOOT_UUID=$(blkid -s UUID -o value $BOOT_PATH)
+    PARTITION_PATH=$(ls -l /dev/disk/by-path | grep $ROOT_PARTITION | cut -d ' ' -f 9)
+    echo "echo \"luks_root /dev/disk/by-path/$PARTITION_PATH none luks,header=/luks_root_header.img:UUID=$BOOT_UUID\" > /mnt/etc/crypttab.initramfs" >> post-install-hook.sh
+    echo "cp luks_root_header.img /mnt/boot" >> post-install-hook.sh
 }
 
 fn_setup_disks() {

ac05aed2b533b75b010099c3222a832506e36ef2
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-05-29 14:48:24 +0200

    Fixed timeshift auto btrfs grub

--- files/timeshift.sh
+++ files/timeshift.sh
@@ -3,4 +3,3 @@
 pacman --noconfirm -S grub-btrfs timeshift timeshift-autosnap
 timeshift --list
 systemctl enable grub-btrfs.path
-sed -ri -e "s/^updateGrub=true.*/updateGrub=false/g" /etc/timeshift-autosnap.conf

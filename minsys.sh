0fd52df4573968023d0b30e3c5fa700f1512d179
Miklós Kovács <fulcrum@kovacsmiki.com>
2022-07-28 09:08:20 +0900

    minsys updated

--- minsys.sh
+++ minsys.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+### Packages
 pacman --noconfirm -Sy archlinux-keyring && pacman-key --populate archlinux
-pacstrap /mnt base btrfs-progs cronie dkms grub linux linux-headers linux-firmware mkinitcpio nano networkmanager sudo which
+pacstrap /mnt base btrfs-progs cronie dkms grub linux linux-headers linux-firmware mkinitcpio nano networkmanager pacman-contrib sudo which
+genfstab -U /mnt > /mnt/etc/fstab
 echo "Defaults editor=/usr/bin/rnano" >> /mnt/etc/sudoers
-arch-chroot /mnt sed -ri -e "s/^#.*%wheel ALL=\(ALL:ALL\) ALL/%wheel ALL=(ALL:ALL) ALL/g" /etc/sudoers
+sed -ri -e "s/^#.*%wheel ALL=\(ALL:ALL\) ALL/%wheel ALL=(ALL:ALL) ALL/g" /mnt/etc/sudoers
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt systemctl enable cronie.service
+
+### User
 while true ; do
     read -p "Username: "
     ! [ -z $REPLY ] && break
@@ -20,3 +26,18 @@ while true ; do
     [ $PASSWORD == $REPLY ] && break
 done
 echo $USERNAME:$PASSWORD | arch-chroot /mnt chpasswd
+
+### Basic system config
+systemd-firstboot --force --root=/mnt --prompt-locale --prompt-keymap --prompt-timezone --prompt-hostname
+sed -ri -e "s/^HOOKS=.*/HOOKS=\(systemd\ keyboard\ modconf\ block\ sd-encrypt\ fsck\ filesystems\)/g" /mnt/etc/mkinitcpio.conf
+sed -ri -e "s/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/g" /mnt/etc/default/grub
+sed -ri -e "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=3/g" /mnt/etc/default/grub
+sed -ri -e "s/^GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/g" /mnt/etc/default/grub
+sed -ri -e "s/^*GRUB_SAVEDEFAULT=.*/GRUB_SAVEDEFAULT=true/g" /mnt/etc/default/grub
+arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=$(cat /mnt/etc/hostname)
+
+
+
+
+
+# microcode packages
